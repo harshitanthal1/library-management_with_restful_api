916e2b831b417f99a6e41dfeb14e7da6f4d18af4
harshit <harshitanthal3@gmail.com>
2024-03-12 05:27:43 +0530

    assignment done

--- .history/models/Book_20240312051520.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const bookSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    status: { type: String, enum: ['available', 'borrowed'], default: 'available' }
-});
-
-module.exports = mongoose.model('Book', bookSchema);
